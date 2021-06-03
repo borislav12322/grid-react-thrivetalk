faa55ed9207d6caf77ad44d43b9b24dced4d80f0
Borislav <borislavizmestiev@gmail.com>
2021-06-03 14:27:00 +0300

    Add adaptive in Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,9 +3,11 @@ import s from './Footer.module.scss';
 
 function Footer(props) {
   return (
+      <footer className={s.footer}>
     <div className={s.wrapper}>
         <small className={s.copyRight} >&copy;  Copyright - ThriveTalk  {props.year}</small>
     </div>
+      </footer>
   );
 }
 export default Footer;
